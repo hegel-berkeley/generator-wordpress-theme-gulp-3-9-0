0f5d7f7b4ec68bd1fa4c6cadcfadb40a9f1b71fa
hchamba-altimea <hegel.chamba@altimea.com>
2022-10-18 21:37:31 -0500

    [Feat]: file example js

---
+++ generators/app/templates/theme/assets/src/js/core.js
@@ -0,0 +1,15 @@
+/* global _, jsVars, jQuery */
+// Use https://lodash.com/docs/4.17.15 from validation in js
+
+( function( $ ) {
+  $( () => {
+    const example= () => {
+      if ( ! _.isEmpty( jsVars ) ) {
+        $( 'body' ).on( 'click', 'a', () => {
+          console.log(jsVars)
+        });
+      }
+    };
+    example();
+  });
+}( jQuery ) );
