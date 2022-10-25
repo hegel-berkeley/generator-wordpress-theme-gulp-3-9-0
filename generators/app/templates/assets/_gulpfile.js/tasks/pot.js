2e1ed89c1bfe19613b12171a80467fd63a78227d
hchamba-altimea <hegel.chamba@altimea.com>
2022-10-25 16:15:21 -0500

    [Feat]: add fies of config

---
+++ generators/app/templates/assets/_gulpfile.js/tasks/pot.js
@@ -0,0 +1,19 @@
+// ==== IMAGES ==== //
+
+var gulp        = require('gulp'),
+    wpPot       = require('gulp-wp-pot'),
+    gulpsort    = require('gulp-sort'),
+    config      = require('../../gulpconfig').theme;
+
+// Generate pot file with new translations
+gulp.task('potfilesgen', function () {
+    return gulp.src(config.php.src)
+        .pipe(gulpsort())
+        .pipe(wpPot( {
+            domain: '<%= name %>-theme',
+            package: '<%= name %>-theme',
+            lastTranslator: '<%= author %> <<%= authorEmail %>>',
+            team: '<%= author %> <<%= authorEmail %>>'
+        } ))
+        .pipe(gulp.dest(config.lang.srcgen + '/<%= name %>-theme.pot'));
+});
